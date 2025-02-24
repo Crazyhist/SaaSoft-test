db05513b15fae0cd720c087ad0bebc2e124c6c0d
Kirill <crazyhistorian2000@yandex.ru>
2025-02-24 16:55:57 +0300

    [feat]: добавлена корректная валидация форм

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,12 @@ import Form from './components/Form.vue'
 </script>
 
 <template>
-	<Form />
+	<div class="container"><Form /></div>
 </template>
 
 <style scoped>
-.logo {
-	height: 6em;
-	padding: 1.5em;
-	will-change: filter;
-	transition: filter 300ms;
-}
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-	filter: drop-shadow(0 0 2em #42b883aa);
+.container {
+	min-height: 100vh;
+	background-color: rgb(230, 230, 230);
 }
 </style>
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,20 +1,24 @@
 <template>
 	<v-container>
-		<h2>Учетные записи</h2>
-		<v-btn color="primary" @click="addAccount">+</v-btn>
+		<h2 class="text-center">Учетные записи</h2>
+		<v-btn color="primary" @click="addAccount" class="mb-4"
+			>Добавить запись</v-btn
+		>
 
 		<v-alert type="info" class="mt-4">
 			Для указания нескольких меток для одной пары логин/пароль используйте
 			разделитель ;
 		</v-alert>
 
-		<v-form ref="form">
+		<v-form ref="form" v-model="valid" lazy-validation>
 			<v-row v-for="(account, index) in accounts" :key="index" class="mt-3">
 				<v-col cols="12" md="3">
 					<v-text-field
 						v-model="account.label"
 						label="Метка"
 						:rules="[(v) => !!v || 'Это поле обязательно']"
+						outlined
+						dense
 					/>
 				</v-col>
 
@@ -23,6 +27,9 @@
 						v-model="account.type"
 						label="Тип записи"
 						:items="['LDAP', 'Локальная']"
+						:rules="[(v) => !!v || 'Выберите тип записи']"
+						outlined
+						dense
 					/>
 				</v-col>
 
@@ -30,23 +37,31 @@
 					<v-text-field
 						v-model="account.login"
 						label="Логин"
-						:rules="[(v) => !!v || 'Это поле обязательно']"
+						:rules="[
+							(v) => !!v || 'Это поле обязательно',
+							(v) => (v && v.length <= 100) || 'Максимум 100 символов',
+						]"
+						outlined
+						dense
 					/>
 				</v-col>
 
-				<v-col cols="12" md="3" v-if="account.type === 'LDAP'">
+				<v-col cols="12" md="3" v-if="account.type === 'Локальная'">
 					<v-text-field
 						v-model="account.password"
 						label="Пароль"
 						type="password"
-						:rules="[(v) => !!v || 'Это поле обязательно']"
+						:rules="[
+							(v) => !!v || 'Это поле обязательно',
+							(v) => (v && v.length <= 100) || 'Максимум 100 символов',
+						]"
+						outlined
+						dense
 					/>
 				</v-col>
 
 				<v-col cols="12" md="1">
-					<v-btn icon @click="removeAccount(index)">
-						<v-icon>mdi-delete</v-icon>
-					</v-btn>
+					<v-btn icon @click="removeAccount(index)"> Х </v-btn>
 				</v-col>
 			</v-row>
 		</v-form>
@@ -60,6 +75,8 @@ const accounts = ref([
 	{ label: '', type: 'Локальная', login: '', password: '' },
 ])
 
+const valid = ref(false)
+
 const removeAccount = (index: number) => {
 	accounts.value.splice(index, 1)
 }
@@ -68,3 +85,27 @@ const addAccount = () => {
 	accounts.value.push({ label: '', type: 'Локальная', login: '', password: '' })
 }
 </script>
+
+<style scoped>
+.text-center {
+	text-align: center;
+}
+
+.v-row {
+	padding: 10px;
+}
+
+.v-text-field,
+.v-select {
+	max-width: 100%;
+}
+
+.v-btn[disabled] {
+	opacity: 0.5;
+}
+
+.v-alert {
+	background-color: #f0f0f0;
+	border-left: 5px solid #00bcd4;
+}
+</style>
